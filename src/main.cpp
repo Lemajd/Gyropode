989d506aa21c2054a966e985136370108c601ebe
Lemajd <djanffardjamel@gmail.com>
2026-02-06 10:13:19 +0100

    calcul de Vs +IHM

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@
 
 /* ===================== PINOUT ===================== */
 #define CLK1 34
-#define DT1  35
+#define DT1 35
 #define CLK2 32
-#define DT2  33
+#define DT2 33
 
 int motor1Pin1 = 23;
 int motor1Pin2 = 19;
@@ -33,33 +33,30 @@ ESP32Encoder encoder_2;
 MPU6050 mpu;
 
 /* ===================== VARIABLES PARTAGÉES ===================== */
-volatile float Ve = 0;   // entrée filtre
-volatile float Vs = 0;   // sortie filtre
+volatile float Ve = 0; // entrée filtre
+volatile float Vs = 0; // sortie filtre
 volatile bool FlagCalcul = false;
 
-float Te = 10;     // ms
-float Tau = 1000;  // ms
+float Te = 10;    // ms
+float Tau = 1000; // ms
 float A, B;
 
-int16_t az, gz;
+int16_t ax, ay, gz;
 float vBAT;
+float angleA;
+float angleG;
 
-/* ===================== TACHE CAPTEURS ===================== */
-void taskCapteurs(void *parameters)
+/* ===================== TACHE Batterie ===================== */
+void taskBatterie(void *parameters)
 {
   TickType_t xLastWakeTime = xTaskGetTickCount();
 
   while (1)
   {
-    az = mpu.getAccelerationZ();
-    gz = mpu.getRotationZ();
-
-    Ve = az;   // entrée du filtre
-
     vBAT = analogRead(ADCpin);
     vBAT = (vBAT * 14.4) / 4095.0;
 
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(5000));
   }
 }
 
@@ -70,7 +67,16 @@ void taskControle(void *parameters)
 
   while (1)
   {
+    ax = mpu.getAccelerationX();
+    ay = mpu.getAccelerationY();
+    gz = mpu.getRotationZ();
+
+    angleA = atan2(float(ay), float(ax));
+    angleG = gz * Tau;
+
+    Ve = angleG + angleA;
     Vs = A * Ve + B * Vs;
+
     FlagCalcul = true;
 
     vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(Te));
@@ -86,11 +92,13 @@ void taskSerial(void *parameters)
     {
       int pos1 = -encoder_1.getCount();
       int pos2 = encoder_2.getCount();
-
+      /*
       Serial.printf(
-        "az:%6d | az_filt:%8.2f | gz:%6d | enc1:%6d | enc2:%6d | Vbat:%4.2f\n",
-        az, Vs, gz, pos1, pos2, vBAT
+        "ax:%6d | ay:%6d | angleA:%6.2f | angleG:%6.2f | enc1:%6d | enc2:%6d | Vbat:%4.2fV\n",
+        ax, ay, angleA, angleG, pos1, pos2, vBAT
       );
+      */
+      Serial.print(Vs);
 
       FlagCalcul = false;
     }
@@ -105,7 +113,7 @@ void setup()
   Serial.println("Systeme demarre");
 
   /* Encodeurs */
-  //ESP32Encoder::useInternalWeakPullResistors = UP;
+  // ESP32Encoder::useInternalWeakPullResistors = UP;
   encoder_1.attachHalfQuad(DT1, CLK1);
   encoder_2.attachHalfQuad(DT2, CLK2);
 
@@ -122,10 +130,12 @@ void setup()
   ledcWrite(pwmChannel_1, dutyCycle);
   ledcWrite(pwmChannel_2, dutyCycle);
 
+  /*
   digitalWrite(motor1Pin1, LOW);
   digitalWrite(motor1Pin2, HIGH);
   digitalWrite(motor2Pin1, HIGH);
   digitalWrite(motor2Pin2, LOW);
+  */
 
   /* I2C + MPU6050 */
   Wire.begin();
@@ -134,7 +144,8 @@ void setup()
   if (!mpu.testConnection())
   {
     Serial.println("MPU6050 NON DETECTE");
-    while (1);
+    while (1)
+      ;
   }
 
   /* ADC */
@@ -146,13 +157,67 @@ void setup()
   B = (Tau / Te) * A;
 
   /* TACHES */
-  xTaskCreate(taskCapteurs, "Capteurs", 6000, NULL, 5, NULL);
+  xTaskCreate(taskBatterie, "Batterie", 6000, NULL, 5, NULL);
   xTaskCreate(taskControle, "Controle", 4000, NULL, 10, NULL);
-  xTaskCreate(taskSerial,   "Serial",   6000, NULL, 1, NULL);
+  xTaskCreate(taskSerial, "Serial", 6000, NULL, 1, NULL);
+}
+
+void reception(char ch)
+{
+
+  static int i = 0;
+  static String chaine = "";
+  String commande;
+  String valeur;
+  int index, length;
+
+  if ((ch == 13) or (ch == 10))
+  {
+    index = chaine.indexOf(' ');
+    length = chaine.length();
+    if (index == -1)
+    {
+      commande = chaine;
+      valeur = "";
+    }
+    else
+    {
+      commande = chaine.substring(0, index);
+      valeur = chaine.substring(index + 1, length);
+    }
+
+    if (commande == "Tau")
+    {
+      Tau = valeur.toFloat();
+      // calcul coeff filtre
+      A = 1 / (1 + Tau / Te);
+      B = Tau / Te * A;
+    }
+    if (commande == "Te")
+    {
+      Te = valeur.toInt();
+      A = 1 / (1 + Tau / Te);
+      B = Tau / Te * A;
+    }
+
+    chaine = "";
+  }
+  else
+  {
+    chaine += ch;
+  }
 }
 
 /* ===================== LOOP ===================== */
 void loop()
 {
   // vide, FreeRTOS travaille
+}
+
+void serialEvent()
+{
+  while (Serial.available() > 0) // tant qu'il y a des caractères à lire
+  {
+    reception(Serial.read());
+  }
 }
